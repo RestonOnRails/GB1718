f5618f53c7d626c73837604a69f8d117613fc067
Diego Quispe <diego@vumble.com>
2016-08-10 20:18:57 -0400

    First commit

---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Event.create(date: "1718-01-07", description: "Occasional Conformity Act repealed")
+Event.create(date: "1718-05-15", description: "James Puckle patents the Puckle Gun")
+Event.create(date: "1718-08-02", description: "Quadruple Alliance formed between Britain, the Kingdom of France, the Holy Roman Empire and the Dutch Republic")
+Event.create(date: "1718-08-11", description: "A British fleet under Admiral Byng defeats a Spanish fleet at the Battle of Cape Passaro, a prelude to the War of the Quadruple Alliance")
